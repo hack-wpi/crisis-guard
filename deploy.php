39187857a0ba1f65af440fb6ed8b82c01986e463
Joshua Hassler <joshua_hassler@student.uml.edu>
2017-01-13 20:36:28 -0500

    Added Deploy Script

---
+++ deploy.php
@@ -0,0 +1,61 @@
+<?php
+namespace Deployer;
+require 'recipe/laravel.php';
+
+// Configuration
+
+set('repository', 'git@github.com:hack-wpi/shltr.git');
+set('http_user', 'ubuntu');
+set('branch', 'master');
+set('keep_releases', 3);
+
+// Servers
+
+server('production', 'hack.symerit.com')
+    ->user('ubuntu')
+    ->identityFile()
+    ->forwardAgent()
+    ->set('deploy_path', '/home/ubuntu/http');
+
+// Tasks
+/**
+ * Deploy start, prepare deploy directory
+ */
+task('deploy:start', function() {
+    cd('~');
+    run("if [ ! -d {{deploy_path}} ]; then mkdir -p {{deploy_path}}; fi");
+    cd('{{deploy_path}}');
+})->setPrivate();
+
+/**Set Storage chmod*/
+task('deploy:storage', function() {
+    cd('{{deploy_path}}');
+    run('chmod -R 777 current/storage');
+})->setPrivate();
+
+/**
+ * Migrate laravel
+ */
+task('deploy:laravel', function() {
+    cd('{{deploy_path}}/current');
+    run("cp /home/ubuntu/configs/.env.deploy ./.env");
+    run("php artisan key:generate");
+    run("php artisan migrate:refresh --seed");
+})->setPrivate();
+
+/**
+ * Main task
+ */
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:vendors',
+    'deploy:symlink',
+    'deploy:storage',
+    'deploy:laravel',
+    'cleanup',
+])->desc('Deploy your project');
+before('deploy:prepare', 'deploy:start');
+after('deploy:shared', 'deploy:writable');
+after('deploy', 'success');
